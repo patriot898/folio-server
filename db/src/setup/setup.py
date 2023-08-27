20702c8da4b4dd9db4324510613240a1c70d2231
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2023-08-26 19:54:04 -0700

    Create draft of sql connection setup

---
+++ db/src/setup/setup.py
@@ -0,0 +1,30 @@
+import psycopg2
+from config import config
+
+
+def create_tables():
+	""" create tables in the PostgreSQL database"""
+	commands = ("../tables/users.sql")
+	conn = None
+	try:
+		# read the connection parameters
+		params = config()
+		# connect to the PostgreSQL server
+		conn = psycopg2.connect(**params)
+		cur = conn.cursor()
+		# create table one by one
+		for command in commands:
+			cur.execute(open(command, "r").read())
+		# close communication with the PostgreSQL database server
+		cur.close()
+		# commit the changes
+		conn.commit()
+	except (Exception, psycopg2.DatabaseError) as error:
+		print(error)
+	finally:
+		if conn is not None:
+			conn.close()
+
+
+if __name__ == '__main__':
+	create_tables()
