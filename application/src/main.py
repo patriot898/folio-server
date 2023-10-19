5c5abe819f44e36c9f1f3a036dc4280e3528a553
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2023-10-18 20:30:50 -0700

    Add deploy_directory and get_user APIs

--- application/src/main.py
+++ application/src/main.py
@@ -1,8 +1,11 @@
 from typing import Union
+from .dal import connect
+from .dal.proc_calls import users
 
 from fastapi import FastAPI
 
 app = FastAPI()
+connector = connect.DBConnector()
 
 
 @app.get("/")
@@ -13,3 +16,15 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/users")
+def get_user(username: str, password: str):
+    row = users.user_get(connector, username, password)
+
+@app.post('/manual_connect/')
+def deploy_directory(directory: str):
+    connector.deploy_directory(directory)
+
+
+
+
