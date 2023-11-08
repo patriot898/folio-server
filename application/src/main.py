5e3d786f5dd9937efe439701443fd2329f920206
Kiefer Ragay <kiefer.m.ragay@gmail.com>
2023-11-08 13:58:29 -0800

    Working commit

--- application/src/main.py
+++ application/src/main.py
@@ -1,11 +1,21 @@
+import os
+from dotenv import load_dotenv
 from typing import Union
 from .dal import connect
 from .dal.proc_calls import users
 
 from fastapi import FastAPI
+from fastapi_sessions import SessionManager
+
+SESSION_KEY = os.getenv('SECRET_KEY')
+SESSION_COOKIE = os.getenv('SESSION_COOKIE_NAME')
 
 app = FastAPI()
 connector = connect.DBConnector()
+session_manager = SessionManager(
+    secret_key=SESSION_KEY,
+    session_cookie=SESSION_COOKIE
+)
 
 
 @app.get("/")
@@ -20,6 +30,13 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.get("/users")
 def get_user(username: str, password: str):
     row = users.user_get(connector, username, password)
+
+@app.post("/login")
+async def login(username: str, password: str):
+    row = users.user_get(connector, username, password)
+    session = await session_manager.create_session()
+    session['user'] = username
+
 
 @app.post('/manual_connect/')
 def deploy_directory(directory: str):
